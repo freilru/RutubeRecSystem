7101c304584db4579c42dbd0dc8cefef6e1f8049
mikhail shafiev <m_shafiev@list.ru>
2024-09-29 05:19:01 +0300

    add readme

--- main/views.py
+++ main/views.py
@@ -1,32 +1,35 @@
+# Импорт необходимых модулей
 import json
-
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-
 from RutubeRecSystem.settings import API_SERVER_URL
 from main.models import CustomUser, Reaction, SearchQuery, ShownVideo
 from main.forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.utils import timezone
 from django.conf import settings
-
 import urllib.request
 import urllib.parse
 
-
-# Create your views here.
-
+# Представление для главной страницы
 def index(request):
+    # Проверка аутентификации пользователя
     if not request.user.is_authenticated:
         return redirect('login')
     
-    # Получаем данные о реакциях пользователя
+    # Получение данных о реакциях пользователя
     liked_videos = Reaction.objects.filter(user=request.user, reaction_type='like').values_list('video_id', flat=True)
+    if not liked_videos:
+        liked_videos = ['478fy4h3fu3']
     disliked_videos = Reaction.objects.filter(user=request.user, reaction_type='dislike').values_list('video_id', flat=True)
+    if not disliked_videos:
+        disliked_videos = ['478fy4h3fu3']
     seen_videos = ShownVideo.objects.filter(user=request.user).values_list('video_id', flat=True)
+    if not seen_videos:
+        seen_videos = ['478fy4h3fu3']
 
-    # Формируем payload для запроса
+    # Формирование payload для запроса к AI серверу
     payload = {
         "user_id": str(request.user.id),
         "items_liked": list(liked_videos),
@@ -34,7 +37,7 @@ def index(request):
         "videos_seen": list(seen_videos)
     }
 
-    # Отправляем запрос на AI сервер
+    # Отправка запроса на AI сервер
     ai_url = API_SERVER_URL + 'recommend_video'
     headers = {'Content-Type': 'application/json'}
     encoded_payload = json.dumps(payload).encode('utf-8')
@@ -43,19 +46,25 @@ def index(request):
     with urllib.request.urlopen(req) as response:
         response_data = json.loads(response.read().decode('utf-8'))
 
-    # Здесь можно обработать полученные рекомендации из response_data
+        # Проверка формата ответа
+        if isinstance(response_data, str):
+            response_data = json.loads(response_data)
 
-    print(response_data)
+        # Обработка полученных рекомендаций
+        recommended_videos = response_data if isinstance(response_data, list) else []
 
-    context = {
-        'recommended_videos': response_data.get('recommended_videos', [])
-    }
+        print(recommended_videos)
 
-    # Обновляем информацию о показанных видео
+        context = {
+            'recommended_videos': recommended_videos
+        }
+    print(context)
+
+    # Обновление информации о показанных видео
     for video in context['recommended_videos']:
         ShownVideo.objects.update_or_create(
             user=request.user,
-            video_id=video['id'],
+            video_id=video['video_id'],
             defaults={'shown_at': timezone.now()}
         )
 
@@ -67,7 +76,7 @@ def index(request):
 
     return render(request, 'main/main-page.html', context)
 
-
+# Представление для регистрации пользователя
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -79,7 +88,7 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'main/register.html', {'form': form})
 
-
+# Представление для входа пользователя
 def user_login(request):
     if request.method == 'POST':
         form = CustomAuthenticationForm(request, data=request.POST)
@@ -94,14 +103,14 @@ def user_login(request):
         form = CustomAuthenticationForm()
     return render(request, 'main/login.html', {'form': form})
 
-
+# Представление для отправки реакции на видео
 @login_required
 def send_reaction(request):
     if request.method == 'POST':
         video_id = request.POST.get('video_id')
         reaction_type = request.POST.get('reaction_type')
 
-        # Создаем новую реакцию или обновляем существующую
+        # Создание новой реакции или обновление существующей
         Reaction.objects.update_or_create(
             user=request.user,
             video_id=video_id,
@@ -110,7 +119,7 @@ def send_reaction(request):
         return JsonResponse({'status': 'success'})
     return JsonResponse({'status': 'error'}, status=400)
 
-
+# Представление для отправки поискового запроса
 @login_required
 def send_search(request):
     if request.method == 'POST':
@@ -119,6 +128,7 @@ def send_search(request):
             SearchQuery.objects.create(user=request.user, query=query)
 
             try:
+                # Отправка запроса на AI сервер для обработки поискового запроса
                 ai_url = API_SERVER_URL + 'search_query'
                 headers = {'Content-Type': 'application/json'}
                 payload = {
@@ -140,11 +150,11 @@ def send_search(request):
             return JsonResponse({'status': 'error', 'message': 'Не удалось сохранить поисковый запрос'}, status=500)
     return JsonResponse({'status': 'error', 'message': 'Неверный метод запроса'}, status=400)
 
-
+# Обработчик ошибки 404
 def custom_404(request, exception):
     return redirect('index')
 
-
+# Представление для профиля пользователя
 @login_required
 def profile(request):
     user = request.user
@@ -160,7 +170,7 @@ def profile(request):
     }
     return render(request, 'main/profile.html', context)
 
-
+# Представление для страницы с короткими видео
 def shorts(request):
     shorts = [
         {'category': ["коты", "животные"], 'video_url': 'http://127.0.0.1:8000/static/main/videos/IMG_4745.MOV'},
@@ -172,18 +182,18 @@ def shorts(request):
         'shorts': shorts,
     }
 
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/shorts.html', context)
 
-
+# Представление для отправки реакции на короткие видео
 @login_required
 def send_shorts_reaction(request):
-    '''отправить на микросервис чтоб обновить рейтинг категорий'''
     if request.method == 'POST':
         video_categories = request.POST.get('video_id')
         reaction_type = request.POST.get('reaction_type')
 
         user_interests = CustomUser.objects.get(id=request.user.id).interests
 
+        # Отправка запроса на AI сервер для обновления рейтинга категорий
         ai_url = API_SERVER_URL + 'process'
         headers = {'Content-Type': 'application/json'}
         payload = {
